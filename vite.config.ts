8034148a76489e7fe70fdfe5193b2d8f2676d017
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-07-01 17:43:47 -0500

    setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
 import path from "path";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "/src"),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    resolve: {
+      base: "",
+      alias: {
+        "~": path.resolve(__dirname, "/src"),
+      },
     },
-  },
-  plugins: [solid()],
+    plugins: [solid()],
+    base: process.env.VITE_BASE_URL || "/",
+  };
 });
