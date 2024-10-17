74579031ab004b780e1815f9865f283e7bee20a3
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-10-17 13:16:50 -0500

    add random filename generation on qr download

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,14 +3,40 @@ import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { resources, TranslationKeys } from '~/lib/i18next';
 import { useTranslationsContext } from '~/components/context/Translations';
+import { NAME_GENERATOR_DICTIONARY } from './const';
+
+function randomNoun() {
+  return NAME_GENERATOR_DICTIONARY.NOUNS[
+    Math.floor(Math.random() * NAME_GENERATOR_DICTIONARY.NOUNS.length)
+  ]!;
+}
+
+function randomAdjective() {
+  return NAME_GENERATOR_DICTIONARY.ADJECTIVES[
+    Math.floor(Math.random() * NAME_GENERATOR_DICTIONARY.ADJECTIVES.length)
+  ]!;
+}
+
+/**
+ * Modified version of https://github.com/thedeveloper/sillyname
+ */
+
+function generateStupidName() {
+  const noun1 = randomNoun();
+  let noun2 = randomNoun();
+  noun2 = noun2.substr(0, 1).toUpperCase() + noun2.substr(1);
+  const adjective = randomAdjective();
+  return adjective + noun1 + ' ' + noun2;
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function download(url: string) {
+export function download(url: string, filename = generateStupidName()) {
   const a = document.createElement('a');
   a.href = url;
-  a.download = url.split('/').pop()!;
+  a.download = filename;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
